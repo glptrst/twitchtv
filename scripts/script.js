4d667b43a8577fe9eea302443df88064149e279c
Giulio <giulio.pietroiusti@gmail.com>
2017-03-22 19:48:22 +0100

    Fetch channels info

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+var channels = ["ESL_SC2", "OgamingSC2", "freecodecamp", "RobotCaleb"];
+
+channels.forEach(function getInfo(channel) {
+    sendReq(channel);
+});
+
+function callback(response) {
+    console.log(response);
+}
+
+function sendReq(channel) {
+    var scriptEl = document.createElement("script");
+    scriptEl.id = "getChannelsInfo";
+    scriptEl.src = "https://wind-bow.glitch.me/twitch-api/channels/" + channel + "?callback=callback";
+    document.body.appendChild(scriptEl);
+    //remove script element
+    document.getElementById(scriptEl.id).remove();
+};
