542d4caa658bbfb3fec278ec3d8d1061f45cc94c
g p <gp@localhost.localdomain>
2017-03-27 19:41:06 +0200

    Display offline and online channels

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,24 @@
-var channels = ["ESL_SC2", "OgamingSC2", "freecodecamp", "RobotCaleb"];
+//channels to get info of
+var channelsNames = ["ESL_SC2", "OgamingSC2", "freecodecamp", "RobotCaleb"];
 
-channels.forEach(function getInfo(channel) {
+//send a jsonp request for each channel
+channelsNames.forEach(function getInfo(channel) {
     sendReq(channel);
 });
 
+//manage responses
 function callback(response) {
-    console.log(response);
+    var channelName = response._links.channel.split("/")[response._links.channel.split("/").length - 1];
+
+    var el = document.getElementById(channelName);
+    if (response.stream === null) {
+        el.innerHTML = channelName +": Offline";
+    } else {
+        el.innerHTML = channelName +": Online!";
+    }
 }
 
+//create jsonp request
 function sendReq(channel) {
     var scriptEl = document.createElement("script");
     scriptEl.id = "getChannelsInfo";
